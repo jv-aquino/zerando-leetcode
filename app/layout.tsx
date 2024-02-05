17c9126139512a7b383182aeb5738b28bce93e9f
João Vitor de Aquino <jvaquinocontact@gmail.com>
2024-02-05 19:46:52 -0300

    + basic setup

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Zerando Leetcode",
+  description: "Site que te ajuda a visualizar as soluções e questões do LeetCode e outros algoritmos",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
